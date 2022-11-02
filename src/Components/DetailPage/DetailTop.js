db6c471b82aa81024fabe23e636b25829640560f
Lee Haewon <hawon2205@ewhain.net>
2022-11-03 00:57:28 +0900

    add DetailPage

---
+++ src/Components/DetailPage/DetailTop.js
@@ -0,0 +1,48 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+import "./DetailPage.css";
+
+const TopContainer = styled.div`
+  display: flex;
+  background: #fff7ef;
+  height: 100vh;
+  width: 100%;
+  
+  position: relative;
+  z-index: 1;
+  scroll-snap-align: start;
+`;
+
+
+function DetailTop(props) {
+  const { id } = useParams(); // 유저가 URL 파라미터에 입력한 거 가져올 때 사용하는 훅
+  let item = props.MainData.find(function (x) {
+    return x.id == id;
+  });
+  return (
+    <>
+    <TopContainer>
+      <div className="detailBG">
+        <div>{item.name}</div>
+        <div>{item.category}</div>
+        <div>{item.address}</div>
+        <div>{item.business_hours}</div>
+        <div>{item.detail_address}</div>
+        <div>{item.star}</div>
+        <div>{item.parking}</div>
+        <div>{item.menu_1}</div>
+        <div>{item.menu_1_price}</div>
+        <div>{item.menu_2}</div>
+        <div>{item.menu_2_price}</div>
+        <div>{item.menu_3}</div>
+        <div>{item.menu_3_pirce}</div>
+        
+      </div>
+      </TopContainer>
+    </>
+  );
+}
+
+
+export default DetailTop;
---
+++ src/Components/DetailPage/Pages.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+
+import { GlobalFonts } from "../../fonts/font";
+import { main_data } from "../MainPage/data";
+import DetailBottom from "./DetailBottom";
+import DetailTop from "./DetailTop";
+
+const PageContainer = styled.div`
+  scroll-snap-typimport React from "react";
+  e: y mandatory;
+  overflow-y: visible;
+  overflow-y: scroll;
+  height: 100vh;
+
+  &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    background: transparent;
+  }
+`;
+
+function DetailPage(){
+    let [MainData, setMainData] = useState(main_data);
+    return(
+        <PageContainer>
+            <GlobalFonts/>
+            <DetailTop MainData={MainData}/>
+            <DetailBottom/>
+        </PageContainer>
+    )
+}
+
+export default DetailPage;
