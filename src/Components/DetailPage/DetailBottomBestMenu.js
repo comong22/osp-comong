d672a91f602cba1baf465fec0e1957d060a99e42
Yu-jimin <jiminyu0114@gmail.com>
2022-11-03 07:51:10 +0900

    add DetailBottomBestMenu bestmenu list

--- src/Components/DetailPage/DetailBottomBestMenu.js
+++ src/Components/DetailPage/DetailBottomBestMenu.js
@@ -1,11 +1,11 @@
 import { GlobalFonts } from "../../fonts/font";
 import styled from "styled-components";
+import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { main_data } from "../MainPage/data";
 
 import "./DetailPage.css";
-import bestmenuimg from '../../images/main/main1.jpg';
-
 
 const BottomContainer = styled.div`
   display: flex;
@@ -15,9 +15,23 @@ const BottomContainer = styled.div`
   scroll-snap-align: start;
 `;
 
-function DetailBottomBestMenu() {
+function DetailBottomBestMenu(props) {
     const [activeNav, setActiveNav] = useState(1);
-  return (
+
+    const bestmenuimg = [
+      {id:0, src:require("../../images/main/main0.jpg")},
+      {id:1, src:require("../../images/main/main1.jpg")},
+      {id:2, src:require("../../images/main/main2.jpg")},
+      {id:3, src:require("../../images/main/main3.jpg")},
+      {id:4, src:require("../../images/main/main4.jpg")},
+      {id:5, src:require("../../images/main/main5.jpg")},
+      {id:6, src:require("../../images/main/main6.jpg")},
+      {id:7, src:require("../../images/main/main7.jpg")},
+      {id:8, src:require("../../images/main/main8.jpg")},
+    ]
+    const { id } = useParams();
+
+    return (
     <>
       <GlobalFonts />
       <BottomContainer>
@@ -25,25 +39,25 @@ function DetailBottomBestMenu() {
           <div className="bottomBox">
             <nav className="BottomNav">
                 <div className={activeNav ===1? "active-nav" : "nav-item"}>
-                <Link to="." onClick={() => setActiveNav(1)}>
+                <a href ="." onClick={() => setActiveNav(1)}>
                     <div> 대표 메뉴 </div>
-                </Link>
+                </a>
                 </div>
                 <div className={activeNav ===2? "active-nav" : "nav-item"}>
-                <Link to="/DetailBottomReview" onClick={() => setActiveNav(2)}>
+                <a href="/DetailBottomReview" onClick={() => setActiveNav(2)}>
                     <div> 리뷰 </div>
-                </Link>
+                </a>
                 </div>
             </nav>
             <div className="navbestfont">
               BEST
             </div>
             <div className="bestmenuImg"> 
-              <img src={bestmenuimg}/>
+              <img src={bestmenuimg[id].src}/>
             </div>
             <div className="navbestmenufont">
-              치즈 돈가스<br/>
-              11,500원
+              {main_data[id].menu_1}<br/>
+              {main_data[id].menu_1_price}
             </div>
             </div>
         </div>
