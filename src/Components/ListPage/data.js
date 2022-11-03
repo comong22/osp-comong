b5897439679d1ee6767f179061154ce6b464f836
jaeryeong <jryeong67@naver.com>
2022-11-03 16:51:45 +0900

    add ListPage data

--- src/Components/ListPage/data.js
+++ src/Components/ListPage/data.js
@@ -22,45 +22,45 @@ export const place01_data = [ //  place01 : 이대 정문
     },
     {
         id: 3,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "원즈오운",
+        address: "서울특별시 서대문구 이화여대길 20 1층",
+        business_hours: "10 : 00 - 21 : 00",
+        signature_menu: "잠봉뵈르"
     },
     {
         id: 4,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "한끼마끼",
+        address: "서울 서대문구 이화여대1길 15 1층",
+        business_hours: "11 : 00 - 20 : 00", // 매주 일 휴무
+        signature_menu: "유부베이직세트"
     },
     {
         id: 5,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "어바웃샤브",
+        address: "서울 서대문구 이화여대8길 2 201호",
+        business_hours: "11 : 00 - 21 : 00",
+        signature_menu: "삼겹 샤브세트"
     },
     {
         id: 6,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "돈천동",
+        address: "서울 서대문구 이화여대길 52-35",
+        business_hours: "10 : 30 - 20 : 30", //주말휴무
+        signature_menu: "김치나베 돈카츠"
     },
     {
         id: 7,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "란주탕슉",
+        address: "서울 서대문구 이화여대2가길 18",
+        business_hours: "12 : 00 - 20 : 30",
+        signature_menu: "사천꿔바로우"
     },
     {
         id: 8,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "유소바",
+        address: "서울 서대문구 이화여대2길 4 1층",
+        business_hours: "11 : 30 - 21 : 30",
+        signature_menu: "덴푸라소바"
     }
 ]
 
@@ -68,65 +68,65 @@ export const place02_data = [ //  place02 : 이대 후문
     {
         id: 0,
         name: "스탠바이키친",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연대동문길 49",
+        business_hours: "10 : 30 - 20 : 00",
+        signature_menu: "베이컨 양상추 토마토"
     },
     {
         id: 1,
         name: "존재의 이유",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울특별시 서대문구 대신동 111-12",
+        business_hours: "10 : 00 - 22 : 00", // 매주 일요일 휴무
+        signature_menu: "가정식 백반"
     },
     {
         id: 2,
         name: "로드샌드위치",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연대동문길 45 1층",
+        business_hours: "10 : 30 - 20 : 00", //매주 일, 월 휴무
+        signature_menu: "감베리(새우) 샌드위치"
     },
     {
         id: 3,
         name: "딸기골",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연대동문길 29",
+        business_hours: "7 : 00 - 21 : 30",
+        signature_menu: "치즈김치순두부"
     },
     {
         id: 4,
         name: "스튜디오웝",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 성산로 539",
+        business_hours: "10 : 00 - 20 : 00", // 매주 일요일 휴무
+        signature_menu: "웝쿠티 (시그니처 수제 밀크티)"
     },
     {
         id: 5,
         name: "하노이의 아침",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연대동문길 45 3층",
+        business_hours: "11 : 00 - 22 : 00", // 주말은 21시까지
+        signature_menu: "차돌양지쌀국수"
     },
     {
         id: 6,
         name: "대치영지",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 성산로 519",
+        business_hours: "11 : 00 - 20 : 00", // 매주 일요일 휴무
+        signature_menu: "닭곰탕"
     },
     {
         id: 7,
         name: "헐리우드",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 성산로 551",
+        business_hours: "10 : 00 - 22 : 00",
+        signature_menu: "크림소스 김치볶음밥"
     },
     {
         id: 8,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "식타",
+        address: "서울 서대문구 성산로 527",
+        business_hours: "11 : 00 - 22 : 00",
+        signature_menu: "와인 소스 스테이크"
     }
 ]
 
@@ -134,64 +134,64 @@ export const place03_data = [ //  place03 : 신촌
     {
         id: 0,
         name: "담산",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연세로5다길 5 1층",
+        business_hours: "11 : 00 - 22 : 00", //매주 월 휴무
+        signature_menu: "양푼 등갈비찜"
     },
     {
         id: 1,
         name: "정육면체",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연세로5다길 22-8 1층",
+        business_hours: "11 : 30 - 21 : 00",
+        signature_menu: "홍탄"
     },
     {
         id: 2,
         name: "정통집",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연세로9길 7 1층",
+        business_hours: "11 : 00 - 23 : 00",
+        signature_menu: "돼지김치구이"
     },
     {
         id: 3,
         name: "돈부리모노",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연세로4길 46 1층",
+        business_hours: "11 : 30 - 21 : 30",
+        signature_menu: "사케동"
     },
     {
         id: 4,
         name: "파이홀",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 연세로5나길 20",
+        business_hours: "11 : 00 - 22 : 00",
+        signature_menu: "얼그레이 가나슈"
     },
     {
         id: 5,
         name: "포티드",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 명물길 36-3 1층",
+        business_hours: "11 : 30 - 22 : 30",
+        signature_menu: "쿠키&아이스크림"
     },
     {
         id: 6,
         name: "미도인",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        address: "서울 서대문구 명물길 33 2층",
+        business_hours: "11 : 30 - 21 : 00",
+        signature_menu: "트리플 스테이크"
     },
     {
         id: 7,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "소신이쏘",
+        address: "서울 서대문구 연세로5가길 19 1층",
+        business_hours: "12 : 00 - 22 : 00",
+        signature_menu: "매운소갈비찜, 크림소갈비찜"
     },
     {
         id: 8,
-        name: "",
-        address: "",
-        business_hours: "",
-        signature_menu: ""
+        name: "고삼이",
+        address: "서울 서대문구 연세로7안길 38",
+        business_hours: "10 : 30 - 20 : 40",
+        signature_menu: "고등어 구이"
     }
 ]
