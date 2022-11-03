b6555e7acd5f18444dbc3e5ecc780c3b4a06cfca
julia-heo <julia_heo@naver.com>
2022-11-03 23:23:31 +0900

    a

--- src/Components/UploadPage/UploadPage.js
+++ src/Components/UploadPage/UploadPage.js
@@ -1,8 +1,24 @@
 import './UploadPage.css';
 
+function Box(props){
+    
+    function AddBox(props){
+        <div style="background: white;height: 50px; width: 200px;border-radius: 15px;width:{props.width};height:{props.width}">
+            <span class="material-symbols-outlined">add_box</span>
+        </div>
+        
+
+	
+}
+
 function UploadPage(){
     return(
         <div className='uploadBG'>
+            <div>
+                <div>
+                    <AddBox width="500px"></AddBox>
+                </div>
+            </div>
             
         </div>
     )
