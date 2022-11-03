07867f38c055765d72334988f2dcdcc78007eeca
leekrkr <smilejuju@ewhain.net>
2022-11-03 18:46:18 +0900

    add topbox

--- src/Components/DetailPage/DetailTop.js
+++ src/Components/DetailPage/DetailTop.js
@@ -18,25 +18,28 @@ const TopContainer = styled.div`
 function DetailTop(props) {
   const { id } = useParams(); // 유저가 URL 파라미터에 입력한 거 가져올 때 사용하는 훅
   let item = props.MainData.find(function (x) {
-    return x.id == id;
+    return x.id === id;
   });
   return (
     <>
     <TopContainer>
       <div className="detailBG">
-        <div>{item.name}</div>
-        <div>{item.category}</div>
-        <div>{item.address}</div>
-        <div>{item.business_hours}</div>
-        <div>{item.detail_address}</div>
-        <div>{item.star}</div>
-        <div>{item.parking}</div>
-        <div>{item.menu_1}</div>
-        <div>{item.menu_1_price}</div>
-        <div>{item.menu_2}</div>
-        <div>{item.menu_2_price}</div>
-        <div>{item.menu_3}</div>
-        <div>{item.menu_3_pirce}</div>
+        <div className="topBox">
+          <div className="navdetailfont">가게 정보</div>
+          <div>{item?.name}</div>
+          <div>{item?.category}</div>
+          <div>{item?.address}</div>
+          <div>{item?.business_hours}</div>
+          <div>{item?.detail_address}</div>
+          <div>{item?.star}</div>
+          <div>{item?.parking}</div>
+          <div>{item?.menu_1}</div>
+          <div>{item?.menu_1_price}</div>
+          <div>{item?.menu_2}</div>
+          <div>{item?.menu_2_price}</div>
+          <div>{item?.menu_3}</div>
+          <div>{item?.menu_3_pirce}</div>
+        </div>
         {/* 리뷰 작성 버튼 필요 -> /review 페이지로 넘어갈 수 있게! */}
       </div>
       </TopContainer>
