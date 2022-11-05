175e6dc633784b665f73a2df76c91c9a1173dbfe
jaeryeong <jryeong67@naver.com>
2022-11-05 12:37:37 +0900

    list

--- src/Components/ListPage/ListStyle.js
+++ src/Components/ListPage/ListStyle.js
@@ -14,6 +14,9 @@ export const ListName = styled.div`
   font-size: 34px;
   line-height: 170%;
   color: #ffba94;
+  text-align: left;
+  font-weight: 700;
+  grid-row-gap: 20px;
 `;
 
 
@@ -34,6 +37,51 @@ export const Column1 = styled.div`
 `;
 
 export const Column2 = styled.div`
+  padding: 0;
+  grid-area: col2;
+`;
+
+export const InsidePage = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 3fr 3fr;
+  grid-template-rows: repeat(9, 1fr);
+  margin-top: 10px;
+  grid-template-areas: 
+  "c1 n1 b"
+  "c1 a1 bh1"
+  "c1 s1 b"
+  "c2 n2 b"
+  "c2 a2 bh2"
+  "c2 s2 b"
+  "c3 n3 b"
+  "c3 a3 bh3"
+  "c3 s3 b"     
+`;
+
+export const Ccolumn1 = styled.div`
+  grid-area: c1;
+`;
+
+export const Ccolumn2 = styled.div`
+  grid-area: c2;
+`;
+
+export const Column3 = styled.div`
+  padding: 0;
+  grid-area: c3;
+`;
+
+export const Row1 = styled.div`
+  padding: 0;
+  grid-area: col2;
+`;
+
+export const Row2 = styled.div`
+  padding: 0;
+  grid-area: col2;
+`;
+
+export const Row3 = styled.div`
   padding: 0;
   grid-area: col2;
 `;
@@ -139,4 +187,32 @@ export const ListBox = styled.div`
   background: #ffffff;
   box-shadow: 0px 4px 22px rgba(0, 0, 0, 0.25);
   border-radius: 25px;
+`;
+
+export const IMGform = styled.div`
+  width: 100%;
+  aspect-ratio: 1;
+`;
+
+export const MapPinIMG = styled.img`
+  float: left;
+  max-width: 22px;
+  margin-right: 10px;
+`;
+export const ClockIMG = styled.img`
+  float: left;
+  max-width: 22px;
+  margin-right: 10px;
+`;
+
+export const InfoText = styled.div`
+  max-width: 1000px;
+  font-family: "Spoqa Han Sans Neo Regular";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  /* or 27px */
+
+  color: #000000;
 `;
