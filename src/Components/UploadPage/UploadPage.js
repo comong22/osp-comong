eac1e9f5421aef24d666c0c11ff85c5a1266f5e8
julia-heo <julia_heo@naver.com>
2022-11-05 18:52:31 +0900

    change style_2

--- src/Components/UploadPage/UploadPage.js
+++ src/Components/UploadPage/UploadPage.js
@@ -15,12 +15,13 @@ function InputBox(props){
     //    setValue(e.target.value);
     //},[]);
     return(
-        <span style={{margin:'30px'}}>
+        <span style={{marginRight:props.margin}}>
         <span className='text'>{props.title}</span>
         <input className="inputText" type='text' size={props.size} defaultValue={props.value} ></input>
         </span>
     )
 }
+
 
 function UploadPage(){
     return(
@@ -33,30 +34,31 @@ function UploadPage(){
             <div style={{display:'flex'}}>
                 <div style={{width: '40%', float: 'left'}}>
                     <AddBox size="400px"></AddBox>
-                    <div className='title'>대표사진</div>
+                    <div className='title' style={{textAlign:"center"}}>대표사진</div>
                 </div>
                <div style={{width: '60%', float: 'right'}}>
                     <div>
-                        <InputBox title="식당이름"></InputBox>
-                        <InputBox title="카테고리" value="일식"></InputBox>
-                        <InputBox title="주차여부" value="Y"></InputBox>
+                        <InputBox  title="식당이름" margin="30px" size="25"></InputBox>
+                        <InputBox title="카테고리" value="일식" margin="40px"></InputBox>
+                        <InputBox title="주차여부" value="Y" size="6"></InputBox>
                     </div>
                     <div style={{marginTop:'20px'}}>
-                        <InputBox title="주소" value="정확한 주소를 입력해주세요!" size='100'></InputBox>
+                        <InputBox title="주소" value="정확한 주소를 입력해주세요!" size='90'></InputBox>
                     </div>
                     <div style={{marginTop:'20px'}}>
-                        <InputBox title="전화번호" value="010-1234-5678" size='20'></InputBox>
-                        <InputBox title="가격대" value="최저가"></InputBox>
-                        <InputBox title="~" value="최고가"></InputBox>
+                        <InputBox title="전화번호" value="010-1234-5678" size='35' margin="40px"></InputBox>
+                        <InputBox title="가격대" value="최저가"size='15'></InputBox>
+                        <InputBox title="~" value="최고가"size='15'></InputBox>
                     </div>
                     <div style={{marginTop:'20px'}}>
-                        <InputBox title="영업시간" value="09:00~21:00" size='40'></InputBox>
-                        <InputBox title="사이트" value="인스타그램, 대표사이트"></InputBox>
+                        <InputBox title="영업시간" value="09:00~21:00" size='18' margin="40px"></InputBox>
+                        <InputBox title="사이트" value="인스타그램, 대표사이트" size='53'></InputBox>
                     </div>
-                    <div className='title'>대표메뉴 등록하기</div>
+                    <hr width="650" style={{position:'absolute',top:'355px',right:'198px',zIndex:"1",backgroundColor:"#FFBA94"}}></hr>
+                    <div  className='title' style={{position:'relative',marginLeft:'220px', backgroundColor:"#FFF7EF",width:"250px", border:"100px",zIndex:"2"}} > 대표메뉴 등록하기</div>
                     <br></br>
-                    <div style={{display:"flex", justifyContent:"center",margin:"0px 90px"}}>
-                        <AddBox style={{float: "left"}} size="150px"></AddBox>
+                    <div style={{display:"flex", justifyContent:"left",margin:"0px 90px"}}>
+                        <AddBox size="150px"></AddBox>
                         <div style={{float: "right"}} >
                             <InputBox title="메뉴명"></InputBox>
                             <br></br>
