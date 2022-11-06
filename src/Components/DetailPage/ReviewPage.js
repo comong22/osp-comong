f3cd086058bcffe4ff07ed2595c78780300a21fd
leekrkr <smilejuju@ewhain.net>
2022-11-06 22:52:10 +0900

    change detail page design

--- src/Components/DetailPage/ReviewPage.js
+++ src/Components/DetailPage/ReviewPage.js
@@ -45,6 +45,7 @@ function Review(){
       const [posts, setPosts] = useState([]);
       const [limit, setLimit] = useState(10);
       const [page, setPage] = useState(1);
+      const offset = (page - 1) * limit;
 
     return(
         <>
