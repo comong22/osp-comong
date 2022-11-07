e165bc445e7121fa18b51ba13b8263acb6ad57fa
julia-heo <julia_heo@naver.com>
2022-11-07 16:20:51 +0900

    add onMouseOver Event

--- src/Components/UploadPage/Modal.js
+++ src/Components/UploadPage/Modal.js
@@ -1,5 +1,13 @@
 import './UploadPage.css';
 
+function mouseOver(e) {
+    e.currentTarget.style.backgroundColor = "#FFBA94";
+}
+function mouseOut(e) {
+    e.currentTarget.style.backgroundColor = "#ffa574";
+} 
+
+
 function ModalBasic({ setModal }) {
     const closeModal = () => {
         setModal(false);
@@ -16,7 +24,7 @@ function ModalBasic({ setModal }) {
                 <p class="t1">맛집을 등록하시겠습니까?</p>
                 <p class="t2">업로드 후에는 수정이 불가능합니다.</p>
                 </div>
-                <button class="submitButton2" style={{marginLeft:"150px",marginTop:"17px"}} onClick={closeModal}>
+                <button class="submitButton2" onMouseOver={(e) =>mouseOver(e)} onMouseOut={(e) =>mouseOut(e)} style={{marginLeft:"150px",marginTop:"17px"}} onClick={closeModal}>
                     맛집 등록하기
                 </button>
 
