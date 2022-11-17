4050d8b09f6e83f7e1dbfe78f60318c780bd7343
Lee Haewon <hawon2205@ewhain.net>
2022-11-17 16:18:31 +0900

    temp

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,20 @@
-import * as firebase from "firebase/app";
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBf0ZjhadxKNtFvTJfL-daI1Ofd72F-cr0",
-    authDomain: "osp-comong.firebaseapp.com",
-    projectId: "osp-comong",
-    storageBucket: "osp-comong.appspot.com",
-    messagingSenderId: "634449059761",
-    appId: "1:634449059761:web:8199305587c9d211a9e790",
-    measurementId: "G-ZRNHF8B8LG"
-
+  apiKey: "AIzaSyBf0ZjhadxKNtFvTJfL-daI1Ofd72F-cr0",
+  authDomain: "osp-comong.firebaseapp.com",
+  projectId: "osp-comong",
+  storageBucket: "osp-comong.appspot.com",
+  messagingSenderId: "634449059761",
+  appId: "1:634449059761:web:8199305587c9d211a9e790",
+  measurementId: "G-ZRNHF8B8LG",
 };
 
-export default firebase.initializeApp(firebaseConfig); // index.js에서 사용을 위함
+// firebaseConfig 정보로 firebase 시작
+const app = initializeApp(firebaseConfig);
+export default {app};
+// firebase의 firestore 인스턴스를 변수에 저장
+const db = getFirestore(app);
