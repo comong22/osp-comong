4cb7440a70cbbfdf614292b1217130ada638cc45
Lee Haewon <hawon2205@ewhain.net>
2022-11-17 19:47:52 +0900

    environment variable setting

--- src/firebase.js
+++ src/firebase.js
@@ -4,17 +4,19 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBf0ZjhadxKNtFvTJfL-daI1Ofd72F-cr0",
-  authDomain: "osp-comong.firebaseapp.com",
-  projectId: "osp-comong",
-  storageBucket: "osp-comong.appspot.com",
-  messagingSenderId: "634449059761",
-  appId: "1:634449059761:web:8199305587c9d211a9e790",
-  measurementId: "G-ZRNHF8B8LG",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // firebaseConfig 정보로 firebase 시작
 const app = initializeApp(firebaseConfig);
-export default {app};
+
 // firebase의 firestore 인스턴스를 변수에 저장
 const db = getFirestore(app);
+
+export default {app, db};
