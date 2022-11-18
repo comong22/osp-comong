4b12f2bb111a665ca6e2eac0d1696688d4494c02
leekrkr <smilejuju@ewhain.net>
2022-11-18 17:03:55 +0900

    firebase 설정 돌려놓기

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
