e2886032b65377411d1f68d054a084879ca6c72f
Lee Haewon <hawon2205@ewhain.net>
2022-11-22 17:52:27 +0900

    change navbar content

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,24 +1,33 @@
-import { LogoImg, Nav, NavBarContainer, NavItem, NavList, NavLogo, NavMenu, NavUpload } from "./NavBarElement";
+import {
+  LogoImg,
+  Nav,
+  NavBarContainer,
+  NavItem,
+  NavList,
+  NavLogin,
+  NavLogo,
+  NavMenu,
+  NavText1,
+  NavText2,
+  NavUpload,
+} from "./NavBarElement";
 import logo from "../../images/logo.png";
-import upload from "../../images/upload.svg";
-import listbtn from "../../images/listbtn.svg";
-function NavBar(){
-
-    return(
-        <Nav>
-            <NavBarContainer>
-                <NavLogo to="/">
-                    <LogoImg src={logo} alt="logo"/>
-                </NavLogo>
-                <NavUpload to="/upload">
-                    <img src={upload} alt="uploadbtn"/>
-                </NavUpload>
-                <NavList to="/list">
-                    <img src={listbtn} alt="listbtn"/>
-                </NavList>
-            </NavBarContainer>
-        </Nav>
-    )
+import user from "../../images/user.svg";
+function NavBar() {
+  return (
+    <Nav>
+      <NavBarContainer>
+        <NavLogo to="/">
+          <LogoImg src={logo} alt="logo" />
+        </NavLogo>
+        <NavUpload to="/upload">맛집 등록</NavUpload>
+        <NavList to="/list">맛집 리스트</NavList>
+        <NavLogin to="/signin">
+          <img src={user} alt="login" />
+        </NavLogin>
+      </NavBarContainer>
+    </Nav>
+  );
 }
 
 export default NavBar;
--- src/Components/NavBar/NavBarElement.js
+++ src/Components/NavBar/NavBarElement.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Link as LinkRouter } from "react-router-dom";
+import { Link, Link as LinkRouter } from "react-router-dom";
 
 export const Nav = styled.nav`
   background: white;
@@ -8,7 +8,7 @@ export const Nav = styled.nav`
   display: flex;
   justify-content: center;
   align-items: center;
-  
+
   position: sticky;
   top: 0;
   z-index: 10;
@@ -25,24 +25,63 @@ export const NavBarContainer = styled.div`
 
 export const NavLogo = styled(LinkRouter)`
   cursor: pointer;
-  display: flex;
+  display: grid;
   align-items: center;
+  margin-right: 40px;
 `;
 
+export const NavText1 = styled.div`
+  display: grid;
+  width: 90px;
+  color: #ffba94;
+  font-size: 20px;
+  font-style: normal;
+  font-family: "Spoqa Han Sans Neo Bold";
+`;
+export const NavText2 = styled.div`
+  display: grid;
+  width: 100px;
+  margin: 0 0 0 20px;
+  color: #ffba94;
+  font-size: 20px;
+  font-style: normal;
+  font-family: "Spoqa Han Sans Neo Bold";
+`;
 export const LogoImg = styled.img`
   width: 150px;
-`
+`;
 
 export const NavUpload = styled(LinkRouter)`
-  margin-left: 82%;
-  margin-right: 3%;
-  display: flex;
-  max-width: 20px;
+  display: grid;
+  max-width: 100px;
   align-items: center;
+  margin: 0 0 0 30px;
+  color: #ffba94;
+  font-size: 20px;
+  font-style: normal;
+  font-family: "Spoqa Han Sans Neo Regular";
+  &:hover {
+    font-family: "Spoqa Han Sans Neo Bold";
+  }
 `;
 
 export const NavList = styled(LinkRouter)`
+  display: grid;
+  max-width: 100px;
+  align-items: center;
+  margin: 0 0 0 30px;
+  color: #ffba94;
+  font-size: 20px;
+  font-style: normal;
+  font-family: "Spoqa Han Sans Neo Regular";
+  &:hover {
+    font-family: "Spoqa Han Sans Neo Bold";
+  }
+`;
+
+export const NavLogin = styled(LinkRouter)`
   display: flex;
   max-width: 50px;
   align-items: center;
+  margin-left: 64%;
 `;
