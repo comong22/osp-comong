8fc614248161b03fc41caf224157cb9fe847f9ed
Lee Haewon <hawon2205@ewhain.net>
2022-11-23 18:23:10 +0900

    change size

--- src/Components/NavBar/ModalElement.js
+++ src/Components/NavBar/ModalElement.js
@@ -4,16 +4,16 @@ import { Link } from "react-scroll";
 export const ModalContainer = styled.div`
   display: grid;
   background-color: white;
-  width: 40%;
+  width: 40vw;
   max-width: 600px;
-  height: 50%;
+  height: 50vh;
   filter: drop-shadow(0px 7px 22px rgba(20, 20, 43, 0.1));
   border-radius: 35px;
   align-content: start;
   overflow-y: auto;
   position: fixed;
-  left: 50%;
-  top: 50%;
+  left: 50vw;
+  top: 50vh;
   padding: 5px;
   transform: translate(-50%, -50%);
   z-index: 1011;
@@ -28,7 +28,7 @@ export const XBtn = styled.img`
 export const LoginContent1 = styled.div`
   display: grid;
   text-align: center;
-  margin: 25px 0 5px 0;
+  margin: 4% 0 5px 0;
   font-family: "Spoqa Han Sans Neo";
   font-style: normal;
   font-weight: 700;
@@ -59,13 +59,13 @@ export const LoginContent2 = styled.div`
 export const Label1 = styled.label`
   font-family: "Spoqa Han Sans Neo Regular";
   font-weight: 10px;
-  margin: 30px 10px 0 18%;
+  margin: 4% 10px 0 18%;
   color: #333333;
 `;
 export const Label2 = styled.label`
   font-family: "Spoqa Han Sans Neo Regular";
   font-weight: 10px;
-  margin: 20px 10px 0 18%;
+  margin: 3% 10px 0 18%;
   color: #333333;
 `;
 
@@ -80,14 +80,14 @@ export const InputBox = styled.input`
   font-family: "Spoqa Han Sans Neo";
   font-weight: 10px;
   text-align: center;
-  margin: 10px 0 0 18%;
+  margin: 2% 0 0 18%;
 `;
 
 export const BtnWrap = styled.div`
   height: 40px;
   display: flex;
 
-  margin-top: 40px;
+  margin-top: 5%;
   align-items: center;
   justify-content: center;
 `;
@@ -123,7 +123,7 @@ export const Minitext = styled.div`
   font-family: "Spoqa Han Sans Neo Regular";
   width: 130px;
   font-size: 15px;
-  margin: 20px 0 0 0%;
+  margin: 3% 0 0 0;
   float: left;
   color: #333333;
 `;
@@ -131,7 +131,7 @@ export const Minitext = styled.div`
 export const SignupText = styled.div`
   font-family: "Spoqa Han Sans Neo Regular";
   font-size: 15px;
-  margin: 20px 0 0 3px;
+  margin: 3% 0 0 3px;
   float: left;
   text-decoration-line: underline;
   color: #ffba94;
