656c7faebf35741098355dbe7d2d72d3af82205c
Yu-jimin <jiminyu0114@gmail.com>
2022-11-25 00:17:31 +0900

    edit uploadpage css

--- src/Components/UploadPage/UploadPage.js
+++ src/Components/UploadPage/UploadPage.js
@@ -108,91 +108,95 @@ function UploadPage() {
         )
     }
 
-        const [modal, setModal] = useState(false);
-        const showModal = () => {
-            setModal(true);
-        };
-        return (
-            <div className='uploadBG'>
-                <SelectBox onClick={() => setShowOptions((prev) => !prev)}>
-                    <Label2>{currentValue}</Label2>
-                    <SelectOptions show={showOptions}>
-                        <Option onClick={() => (setCurrentValue("이대 정문"), setPlace(0))}>
-                            이대 정문
-                        </Option>
-                        <Option onClick={() => (setCurrentValue("이대 후문"), setPlace(1))}>
-                            이대 후문
-                        </Option>
-                        <Option onClick={() => (setCurrentValue("신촌"), setPlace(2))}>
-                            신촌
-                        </Option>
-                    </SelectOptions>
-                </SelectBox>
-                <div className='item'>
-                    <AddBox size="400px">
-                        <UploadImg>
-                            <form onSubmit={formHandler}>
-                                <input type="file" className="input" />
-                                <button type="submit">Upload</button>
-                            </form>
-                        </UploadImg>
-                    </AddBox>
-                    <div className='title' style={{ marginLeft: "80px", marginTop: "20px" }}>대표사진</div>
+    const [modal, setModal] = useState(false);
+    const showModal = () => {
+        setModal(true);
+    };
+    return (
+        <div className='uploadBG'>
+
+            <div className='item'>
+                <AddBox size="400px">
+                    <UploadImg>
+                        <form onSubmit={formHandler}>
+                            <input type="file" className="input" />
+                            <button type="submit">Upload</button>
+                        </form>
+                    </UploadImg>
+                </AddBox>
+                <div className='title' style={{ marginLeft: "80px", marginTop: "20px" }}>대표사진</div>
+            </div>
+            <div className='item' >
+                <div className='selectbox'>
+                    <SelectBox onClick={() => setShowOptions((prev) => !prev)}>
+                        <Label2>{currentValue}</Label2>
+                        <SelectOptions show={showOptions}>
+                            <Option onClick={() => (setCurrentValue("이대 정문"), setPlace(0))}>
+                                <span className='selecttext'>이대 정문</span>
+                            </Option>
+                            <Option onClick={() => (setCurrentValue("이대 후문"), setPlace(1))}>
+                                <span className='selecttext'>이대 후문</span>
+                            </Option>
+                            <Option onClick={() => (setCurrentValue("신촌"), setPlace(2))}>
+                                <span className='selecttext'>신촌</span>
+                            </Option>
+                        </SelectOptions>
+                    </SelectBox>
                 </div>
-                <div className='item' >
+                <div>
+                    <InputBox title="식당이름" name="name" onChange={(e) => { setName(e.target.value) }} margin="30px" size="25"></InputBox>
+                    <InputBox title="카테고리" name="cate" onChange={(e) => { setCate(e.target.value) }} value="일식" margin="40px"></InputBox>
+                    <InputBox title="주차여부" name="park" onChange={(e) => { setPark(e.target.value) }} value="Y" size="6"></InputBox>
+                </div>
+                <div style={{ marginTop: '20px' }}>
+                    <InputBox title="주소" name="addr" onChange={(e) => { setAddr(e.target.value) }} value="정확한 주소를 입력해주세요!" size='90'></InputBox>
+                </div>
+                <div style={{ marginTop: '20px' }}>
+                    <InputBox title="전화번호" name="tel" onChange={(e) => { setTel(e.target.value) }} value="010-1234-5678" size='35' margin="40px"></InputBox>
+                    <InputBox title="가격대" name="price1" onChange={(e) => { setPrice1(e.target.value) }} value="최저가" size='15'></InputBox>
+                    <InputBox title="~" name="price2" onChange={(e) => { setPrice2(e.target.value) }} value="최고가" size='15'></InputBox>
+                </div>
+                <div style={{ marginTop: '20px' }}>
+                    <InputBox title="영업시간" name="time" onChange={(e) => { setTime(e.target.value) }} value="09:00~21:00" size='18' margin="40px"></InputBox>
+                    <InputBox title="사이트" name="site" onChange={(e) => { setSite(e.target.value) }} value="인스타그램, 대표사이트" size='53'></InputBox>
+                </div>
+
+                <div className='HR'>
+                    <div ><hr className='hr1'></hr></div>
+                    <div className='title' > 대표메뉴 등록하기</div>
+                    <div ><hr className='hr2'></hr></div>
+                </div>
+
+                <div className='addMenu' style={{ justifyContent: "center" }}>
+                    <div className='it1'>
+                        <AddBox size="150px"></AddBox>
+                    </div>
                     <div>
-                        <InputBox title="식당이름" name="name" onChange={(e) => { setName(e.target.value) }} margin="30px" size="25"></InputBox>
-                        <InputBox title="카테고리" name="cate" onChange={(e) => { setCate(e.target.value) }} value="일식" margin="40px"></InputBox>
-                        <InputBox title="주차여부" name="park" onChange={(e) => { setPark(e.target.value) }} value="Y" size="6"></InputBox>
+                        <div style={{ margin: "35px" }}></div>
+                        <InputBox title="메뉴명" name="bestmenuname" onChange={(e) => { setBestmenuname(e.target.value); }}></InputBox>
                     </div>
-                    <div style={{ marginTop: '20px' }}>
-                        <InputBox title="주소" name="addr" onChange={(e) => { setAddr(e.target.value) }} value="정확한 주소를 입력해주세요!" size='90'></InputBox>
+                    <div>
+                        <InputBox title="가격" name="bestmenuprice" onChange={(e) => { setBestmenuprice(e.target.value); }} size="15" ></InputBox>
                     </div>
-                    <div style={{ marginTop: '20px' }}>
-                        <InputBox title="전화번호" name="tel" onChange={(e) => { setTel(e.target.value) }} value="010-1234-5678" size='35' margin="40px"></InputBox>
-                        <InputBox title="가격대" name="price1" onChange={(e) => { setPrice1(e.target.value) }} value="최저가" size='15'></InputBox>
-                        <InputBox title="~" name="price2" onChange={(e) => { setPrice2(e.target.value) }} value="최고가" size='15'></InputBox>
-                    </div>
-                    <div style={{ marginTop: '20px' }}>
-                        <InputBox title="영업시간" name="time" onChange={(e) => { setTime(e.target.value) }} value="09:00~21:00" size='18' margin="40px"></InputBox>
-                        <InputBox title="사이트" name="site" onChange={(e) => { setSite(e.target.value) }} value="인스타그램, 대표사이트" size='53'></InputBox>
-                    </div>
-
-                    <div className='HR'>
-                        <div ><hr className='hr1'></hr></div>
-                        <div className='title' > 대표메뉴 등록하기</div>
-                        <div ><hr className='hr2'></hr></div>
-                    </div>
-
-                    <div className='addMenu' style={{ justifyContent: "center" }}>
-                        <div className='it1'>
-                            <AddBox size="150px"></AddBox>
-                        </div>
-                        <div>
-                            <div style={{ margin: "35px" }}></div>
-                            <InputBox title="메뉴명" name="bestmenuname" onChange={(e) => { setBestmenuname(e.target.value); }}></InputBox>
-                        </div>
-                        <div>
-                            <InputBox title="가격" name="bestmenuprice" onChange={(e) => { setBestmenuprice(e.target.value); }} size="15" ></InputBox>
-                        </div>
-                        <div className='it4'>
-                            <Button onClick={ () => 
-                                bucket
-                                 .add({ name, cate, park, addr, tel, price1, price2, time, site, bestmenuname, bestmenuprice 
-                                 })
-                                 .then((docRef) => { 
+                    <div className='it4'>
+                        <Button onClick={() =>
+                            bucket
+                                .add({
+                                    name, cate, park, addr, tel, price1, price2, time, site, bestmenuname, bestmenuprice
+                                })
+                                .then((docRef) => {
                                     console.log(docRef.id)
-                                  })
-                                }
-                                   >등록하기
-                                   </Button>
-                            {modal && <ModalBasic setModal={setModal} />}
-                        </div>
+                                })
+                        }
+                        >등록하기
+                        </Button>
+                        {modal && <ModalBasic setModal={setModal} />}
                     </div>
                 </div>
             </div>
+        </div>
 
-        );
+    );
 }
 
 export default UploadPage;
